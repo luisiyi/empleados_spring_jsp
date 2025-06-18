8ecb30fbda182efac2b12ac77b45d2d1c6266b31
LAGM <luisiyi1997@gmail.com>
2025-06-17 20:39:15 -0700

    Editar method indexControlador
    -added editar method implementation to edit an empleado, we receive a modelAttribute "empleadoForma" this comes from the front end with all the fields values of the empleado selected, then we call empleadoServicio.guardarEmpleado(empleado) to save it or this case update it.(Hibernate makes the distinction when the model does not have the id or not, if it has it it will update, if not, will add it.), and finally redirect to the "/" method to recharge all the empleados and redirect it to the index.jsp too.

--- src/main/java/gm/empleados/controlador/IndexControlador.java
+++ src/main/java/gm/empleados/controlador/IndexControlador.java
@@ -52,4 +52,11 @@ public class IndexControlador {
         return "editar";//mostrar editar.jsp
     }
 
+    @RequestMapping(value = "/editar", method = RequestMethod.POST)
+    public String editar(@ModelAttribute("empleadoForma") Empleado empleado){
+        logger.info("Empleado a editar: " + empleado);
+        empleadoServicio.guardarEmpleado(empleado);
+        return "redirect:/"; //redirigimos al controlador "/"
+    }
+
 }
